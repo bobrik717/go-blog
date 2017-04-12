9b7fab55e7bbc553a6e6176851d8f053fbbabbcc
job <bobrik717@gmail.com>
2017-04-12 09:37:38 +0300

    rewrite and del

--- main.go
+++ main.go
@@ -17,7 +17,9 @@ func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/",http.FileServer(http.Dir("./assets/"))))
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/write", writeHandler)
+	http.HandleFunc("/edit", editHandler)
 	http.HandleFunc("/savePost", savePostHandler)
+	http.HandleFunc("/deletePost", deletePostHandler)
 
 	http.ListenAndServe(":3000",nil)
 }
@@ -38,13 +40,49 @@ func writeHandler(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w,"write",nil)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("views/write.html","views/header.html","views/footer.html")
+	if err != nil {
+		fmt.Fprintf(w,err.Error())
+	}
+
+	id := r.FormValue("id")
+
+	pots, isFound := posts[id]
+	if !isFound {
+		http.NotFound(w,r)
+	}
+
+	temp.ExecuteTemplate(w,"write", pots)
+}
+
 func savePostHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.FormValue("id")
 	title := r.FormValue("title")
 	content := r.FormValue("content")
 
-	post := models.NewPost(id,title,content)
-	posts[post.Id] = post
+	if id == "" {
+		id = models.GenerateId()
+		post := models.NewPost(id,title,content)
+		posts[post.Id] = post
+	} else {
+		_, isFound := posts[id]
+		if !isFound {
+			http.NotFound(w,r)
+		}
 
+		posts[id].Title = title
+		posts[id].Content = content
+	}
+	http.Redirect(w,r,"/",302)
+}
+
+func deletePostHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	_, isFound := posts[id]
+	if !isFound {
+		http.NotFound(w,r)
+	}
+	delete(posts, id)
 	http.Redirect(w,r,"/",302)
 }
